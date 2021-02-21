a2312df9e4ace3d71509f510627cb9fc2aefe5a9
Ammon Victor <ammon.victor@gmail.com>
2021-02-21 19:00:28 +0300

    folder restructure for servers applications and web applications

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const pdfjs = require('pdfjs-dist/es5/build/pdf')
 const _ = require("lodash")
 
-pdfjs.getDocument("./bank-statement.pdf").promise
+pdfjs.getDocument("../bank-statement.pdf").promise
     .then(async (doc) => {
         let page = await doc.getPage(1) // if doc has many pages use doc.numPages to iterate and pass index to doc.getPage
         let content = await page.getTextContent()
+        console.log(content.items)
 
         return content.items
             .filter((item) => item.str.trim().length)

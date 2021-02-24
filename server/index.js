5c7d017d690210a393ca48db4277904825c4264e
Ammon Victor <ammon.victor@gmail.com>
2021-02-24 08:57:58 +0300

    Extracted the code into smaller functions

--- server/index.js
+++ server/index.js
@@ -1,24 +1,40 @@
 const pdfjs = require('pdfjs-dist/es5/build/pdf')
 const _ = require("lodash")
 
-pdfjs.getDocument("./bank-statement.pdf").promise
-    .then(async (doc) => {
-        let page = await doc.getPage(1) // if doc has many pages use doc.numPages to iterate and pass index to doc.getPage
-        let content = await page.getTextContent()
-        console.log(content.items)
+async function getContent(src) {
+    const doc = await pdfjs.getDocument(src).promise
+    const page = await doc.getPage(1) // if doc has many pages use doc.numPages to iterate and pass index to doc.getPage
+    return await page.getTextContent()
+}
 
-        return content.items
-            .filter((item) => item.str.trim().length)
-            .map((item) => item.str)
-            .filter((item, i) => i > 7)
-    })
-    .then(items => {
-        const records = _.chunk(items, 6)
-            .reduce((records, [id, date, amount, description, reconciled, transaction_type]) => {
-                records.push({ id, date, amount, description, reconciled, transaction_type })
-                return records
-            }, [])
-        // save json or save csv or write to db
-        console.log(records)
-    })
-    .catch(error => { console.log(error) }) // handle errors
+async function getItems(src) {
+    // Perform pre-processing
+    const content = await getContent(src)
+    // console.log(content.items)
+
+    return content.items
+        .filter((item) => item.str.trim().length)
+        .map((item) => item.str)
+        .filter((item, i) => i > 7)
+}
+
+
+function processItems(items) {
+    const records = _.chunk(items, 6)
+        .reduce((records, [id, date, amount, description, reconciled, transaction_type]) => {
+            records.push({ id, date, amount, description, reconciled, transaction_type })
+            return records
+        }, [])
+
+    // save json or save csv or write to db
+    console.log(records)
+}
+
+function handleErrors(error) {
+    // handle errors
+    console.log(error)
+}
+
+getItems("./bank-statement.pdf")
+    .then(processItems)
+    .catch(handleErrors)
